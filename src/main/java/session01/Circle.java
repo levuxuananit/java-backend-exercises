66f711f36e7ea0390405ef9dae913e43a32efe5c
LeVuXuanAn <levuxuanan.work@gmail.com>
2026-03-13 09:18:15 +0700

    add session 01 - lab 01

---
+++ src/main/java/session01/Circle.java
@@ -0,0 +1,12 @@
+package session01;
+
+public class Circle {
+    double radius;
+
+//    public Circle(double radius){};
+
+
+    public double getArea (double radius){
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
---
+++ src/main/java/session01/Test.java
@@ -0,0 +1,12 @@
+package session01;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main (String[] args) {
+        Circle circle = new Circle();
+        Scanner sc = new Scanner(System.in);
+        circle.radius = sc.nextDouble();
+        System.out.println( String.format("Area of the circle: %.2f", circle.getArea(circle.radius)));
+    }
+}
